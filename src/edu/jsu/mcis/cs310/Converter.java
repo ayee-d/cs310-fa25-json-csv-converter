10b958b0107dd94521e15e226bb96da1d94d3600
ayee-d <bigdt8616@gmail.com>
2025-09-20 11:52:29 -0500

    Completed CSV and Json converters

--- src/edu/jsu/mcis/cs310/Converter.java
+++ src/edu/jsu/mcis/cs310/Converter.java
@@ -1,7 +1,18 @@
 package edu.jsu.mcis.cs310;
 
-import com.github.cliftonlabs.json_simple.*;
-import com.opencsv.*;
+import com.github.cliftonlabs.json_simple.JsonArray;
+import com.github.cliftonlabs.json_simple.Jsoner;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+import java.io.StringReader;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.List;
+import java.io.StringWriter;
+
+
+
 
 public class Converter {
     
@@ -9,7 +20,8 @@ public class Converter {
         
         Consider the following CSV data, a portion of a database of episodes of
         the classic "Star Trek" television series:
-        
+    
+    
         "ProdNum","Title","Season","Episode","Stardate","OriginalAirdate","RemasteredAirdate"
         "6149-02","Where No Man Has Gone Before","1","01","1312.4 - 1313.8","9/22/1966","1/20/2007"
         "6149-03","The Corbomite Maneuver","1","02","1512.2 - 1514.1","11/10/1966","12/9/2006"
@@ -68,6 +80,7 @@ public class Converter {
         STRINGS!!!  Leave ALL string conversion to the two data conversion
         libraries we have discussed, OpenCSV and json-simple.  See the "Data
         Exchange" lecture notes for more details, including examples.
+    
         
     */
     
@@ -77,9 +90,51 @@ public class Converter {
         String result = "{}"; // default return value; replace later!
         
         try {
-        
-            // INSERT YOUR CODE HERE
             
+            CSVReader reader = new CSVReader(new StringReader(csvString));
+            
+            String[] header = reader.readNext();
+            if (header == null) {
+                reader.close();
+                return result.trim();
+            }
+            
+            JsonArray colHeadings = new JsonArray();
+            for (String h : header) {
+                colHeadings.add(h);
+            }
+            
+            
+            JsonArray prodNums = new JsonArray();
+            JsonArray data = new JsonArray();
+            
+            String[] row;
+            while ((row = reader.readNext()) != null) {
+                if (row.length == 0) continue;
+                
+                prodNums.add(row[0]);
+                
+                JsonArray one = new JsonArray();
+                one.add(row[1]);
+                one.add(Integer.parseInt(row[2].trim()));
+                one.add(Integer.parseInt(row[3].trim()));
+                one.add(row[4]);
+                one.add(row[5]);
+                one.add(row[6]);
+                data.add(one);
+            }
+            reader.close();
+            
+            
+            Map<String, Object> root = new LinkedHashMap();
+                root.put("ProdNums", prodNums);
+                root.put("ColHeadings", colHeadings);
+                root.put("Data", data);
+                
+                result = Jsoner.serialize(root);
+            
+        
+               
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -96,9 +151,51 @@ public class Converter {
         
         try {
             
-            // INSERT YOUR CODE HERE
+            Map<String, Object> root = (Map<String, Object>) Jsoner.deserialize(jsonString);
             
+            List<?> colHeadings = (List<?>) root.get("ColHeadings");
+            List<?> prodNums = (List<?>) root.get("ProdNums");
+            List<?> data = (List<?>) root.get("Data");
+            
+            StringWriter out = new StringWriter();
+            CSVWriter writer = new CSVWriter(out);
+            
+            String[] header = new String[colHeadings.size()];
+            for (int i = 0; i < colHeadings.size(); i++){
+                header[i] = String.valueOf(colHeadings.get(i));
+                
+            }
+            writer.writeNext(header, true);
+        
+            for (int i = 0; i < data.size(); i++) {
+                List<?> row = (List<?>) data.get(i);
+                
+                String prod = String.valueOf(prodNums.get(i));
+                String title = String.valueOf(row.get(0));
+                
+                int season = ((Number) row.get(1)).intValue();
+                int episode = ((Number) row.get(2)).intValue();
+                
+                String seasonStr = String.valueOf(season);
+                String episodeStr = (episode < 10) ? ("0" + episode) : String.valueOf(episode);
+                String stardate = String.valueOf(row.get(3));
+                String original = String.valueOf(row.get(4));
+                String remaster = String.valueOf(row.get(5));
+                
+                writer.writeNext(
+                        new String[]{ prod, title, seasonStr, episodeStr, stardate, original, remaster}, true);
+                
+                        
+            }
+            
+            writer.flush();
+            writer.close();
+            result = out.toString();
+            
+         
+          
         }
+        
         catch (Exception e) {
             e.printStackTrace();
         }
